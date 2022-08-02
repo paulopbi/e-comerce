84f173671ef6bfecf83113b97093345e893e821a
Paulo Victor <paulpbi16@gmail.com>
2022-08-01 21:38:14 -0300

    modificado: foi criado as funcoes que abrem e fecham o modal

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
 const year = document.querySelector(".date");
+const modalContainer = document.querySelector(".out-modal-container");
+const womanCard = document.querySelector(".products-card-woman");
+const kidCard = document.querySelector(".products-card-kid");
+const modalCloseBtn = document.querySelector(".out-modal__close");
 
-// !select the date class and fill the innerHTML with the current year in the copyright footer.
+// !copyRightFunction - select the date class and fill the innerHTML with the current year in the copyright footer.
 function copyRightYear() {
-  year.innerHTML = new Date().getFullYear() + ".";
+  return year.innerHTML = new Date().getFullYear() + ".";
 }
 
 copyRightYear();
+
+// !openModal - open the modal, add click event listener and when you click in the link/div it will open the 'out of stock' message.
+function openModal(event) {
+  event.preventDefault();
+  modalContainer.classList.add("show");
+}
+
+womanCard.addEventListener("click", openModal);
+kidCard.addEventListener("click", openModal);
+
+// !closeModal - it will close the modal when you click on modal btn 'fechar/close'.
+function closeModal(event) {
+  event.preventDefault();
+  modalContainer.classList.remove("show");
+}
+
+modalCloseBtn.addEventListener("click", closeModal);
