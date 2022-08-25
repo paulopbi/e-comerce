ae9a66128ebb1826c9029f51682865a7649cbd51
Paulo Victor <paulpbi16@gmail.com>
2022-08-24 22:40:34 -0300

    modificado: mudança na estrutura de pastas da pagina masculino

---
+++ pages/masculino/src/js/masculino.js
@@ -0,0 +1,32 @@
+const elements = {
+  dragonBallBtn: document.querySelector(".products-card-dragonball__button"),
+  buyModal: document.querySelector(".buy-modal"),
+  buyModalClose: document.querySelector(".buy-modal-close__btn"),
+  copyRightYear: document.querySelector(".date"),
+  buyNowBtn: document.querySelector(".buy-modal__btn"),
+
+  addModalClick: (event) => {
+    //?add the classlist 'show' then add display block on css.
+    event.preventDefault();
+    elements.buyModal.classList.add("show");
+  },
+
+  closeBtnClick: (event) => {
+    //?remove the classlist 'show' then it will be display 'none' on css.
+    event.preventDefault();
+    elements.buyModal.classList.remove("show");
+  },
+
+  alertMsg: () => 
+    //?return the alert with a 'thank you' message.
+    window.alert("MUITO OBRIGADO POR TESTAR O MEU SITE!!"),
+
+  copyRight: () =>
+    //?copyRight - return the year and fill it on footer innerHTML.
+    (elements.copyRightYear.innerHTML = new Date().getFullYear() + "."),
+};
+
+elements.dragonBallBtn.addEventListener("click", elements.addModalClick);
+elements.buyModalClose.addEventListener("click", elements.closeBtnClick);
+elements.buyNowBtn.addEventListener("click", elements.alertMsg);
+elements.copyRight();
